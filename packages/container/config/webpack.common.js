556e36de38f860320a89f122b2b457392a9947a7
A-L-M <andreslopezmartinez0@gmail.com>
2025-03-26 18:26:48 -0600

    Created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     module: {
         rules: [
@@ -16,5 +18,10 @@ module.exports = {
     },
     resolve: {
         extensions: [".js", ".jsx"],
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
 };
